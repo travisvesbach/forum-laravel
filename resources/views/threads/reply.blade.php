5ec98b9aa382559707946364cbcd47f21167afec
Travis <travis@webspecdesign.com>
2018-04-09 16:30:42 -0500

    Finish lesson 19

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,21 @@
 <div class="card form-group">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+    	<div class="level">
+    		<h6 class="flex">
+		        <a href="#">
+		            {{ $reply->owner->name }}
+		        </a> said {{ $reply->created_at->diffForHumans() }}...
+			</h6>
+	        <div>
+	        	<form method="POST" action="/replies/{{ $reply->id }}/favorites">
+	        		{{ csrf_field() }}
+	        		<button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }} >
+	        			{{ $reply->favorites()->count() }} {{ str_plural('Favorite',  $reply->favorites()->count() ) }}
+	        		</button>
+	        		
+				</form>
+	        </div>
+    	</div>
     </div>
 
     <div class="card-body">
