562ac2b8c11030613d69fdd86b313a721620ed39
Travis <travis@webspecdesign.com>
2018-04-09 16:04:13 -0500

    Finish lesson 18

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::get('/threads/{channel}', 'ThreadsController@index');
 
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 
+Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
